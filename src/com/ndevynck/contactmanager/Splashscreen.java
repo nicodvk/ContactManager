27118c71e27e25578ca1e892c196706679da5545
Nicolas Devynck <nicolas.devynck@epsi.fr>
2014-03-03 15:27:36 +0100

    Add Splashscreen

---
+++ src/com/ndevynck/contactmanager/Splashscreen.java
@@ -0,0 +1,24 @@
+package com.ndevynck.contactmanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+ 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
